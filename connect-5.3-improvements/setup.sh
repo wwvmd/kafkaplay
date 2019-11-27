599e9d68e715569426f2289e9118273e69634540
wwvmd <wwvmd@devnull>
2019-11-27 12:22:28 +0000

    added setup scripts

---
+++ connect-5.3-improvements/setup.sh
@@ -0,0 +1,2 @@
+export CONNECT_SSL_SECRETS_DIR=/c/dev/confluent/cp-docker-images/examples/kafka-cluster-ssl/client/
+export KAFKA_SSL_SECRETS_DIR=/c/dev/confluent/cp-docker-images/examples/kafka-cluster-ssl/secrets/
